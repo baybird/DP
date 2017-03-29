2799b94a7dd7e0ee67c04734b83cfe6044bc6c3a
baybird <bayareabird@gmail.com>
2017-03-29 09:59:50 -0700

    Finish Singleton pattern

---
+++ 01.singleton.py
@@ -0,0 +1,30 @@
+# Filename      : 01.singleton.py
+# Author        : Robert.Tang
+# Created       : 3/29/2017
+# Last Modified :
+# Description   : Singleton Pattern of Creational Patterns
+# Python Version: 2.7
+#
+
+
+class Singleton:
+  instance = None
+
+  @staticmethod
+  def getInstance():
+    if Singleton.instance == None:
+      Singleton.instance = Singleton()
+
+    return Singleton.instance
+
+
+# Test
+obj = Singleton.getInstance()
+print obj;
+
+obj2 = Singleton.getInstance()
+print obj2;
+
+# Result:
+# <__main__.Singleton instance at 0x02D91710>
+# <__main__.Singleton instance at 0x02D91710>
